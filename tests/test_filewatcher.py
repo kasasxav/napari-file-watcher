bdb8b5c09500cf3fc4127ef33c336dc98979b5d2
Xavier <35917480+kasasxav@users.noreply.github.com>
2022-10-25 18:01:39 +0200

    Change run folder (copy not allowed)

--- tests/test_filewatcher.py
+++ tests/test_filewatcher.py
@@ -4,24 +4,24 @@ import shutil
 import os
 
 
-def test_browse(widget=None):
+def test_browse(widget=None, path=None):
     if not widget:
         widget = WatcherWidget(napari.Viewer(show=False))
     try:
         os.mkdir('example_data/run')
     except FileExistsError:
         pass
-    path = 'example_data/run'
+    if not path:
+        path = 'example_data/run'
     widget.browse(path=path)
 
 
 def test_new_files(qtbot, widget=None):
     if not widget:
         widget = WatcherWidget(napari.Viewer(show=False))
-    test_browse(widget)
-    widget.toggleWatch(True)
     with qtbot.waitSignal(widget.watcher.sigNewFiles):
-        shutil.copy('example_data/neuron_tile_8.zarr', 'example_data/run/neuron_tile_8.zarr')
+        test_browse(widget=widget, path='example_data/')
+        widget.toggleWatch(True)
     widget.showMetadata('neuron_tile_8.zarr')
     try:
         os.rmdir('example_data/run')
