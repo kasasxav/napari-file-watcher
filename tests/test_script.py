82b8a65770e202782800d8ea21be4721fc22fdc6
Xavier <35917480+kasasxav@users.noreply.github.com>
2022-10-25 17:18:32 +0200

    Update test_script.py

--- tests/test_script.py
+++ tests/test_script.py
@@ -17,7 +17,6 @@ def test_browse(widget=None):
     os.mkdir('example_data/run')
     path = 'example_data/run'
     widget.browse(path=path)
-    os.rmdir('example_data/run')
 
 
 def test_add():
@@ -25,7 +24,6 @@ def test_add():
     test_open(widget)
     test_browse(widget)
     widget.add()
-    os.mkdir('example_data/run')
     assert(filecmp.cmpfiles('example_data/', 'example_data/run/', ['timelapse.py', 'experiment.py']))
     os.remove('example_data/run/experiment.py')
     os.rmdir('example_data/run')
